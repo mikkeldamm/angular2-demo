bb2cea18b6bf2a5739ef3b74e7c31552b7d844c0
mikkeldamm <mikkeldamm@hotmail.com>
2015-11-19 22:47:15 +0100

    Added basic 2 way binding for offercode input

--- src/components/offerCode.ts
+++ src/components/offerCode.ts
@@ -1,11 +1,14 @@
-import {Component} from "angular2/angular2";
+import {Component, FORM_DIRECTIVES} from "angular2/angular2";
 
 @Component({
 	selector: 'offer-code',
-	templateUrl: './src/components/offerCode.html'
+	templateUrl: './src/components/offerCode.html',
+	directives: [FORM_DIRECTIVES]
 })
 
 export class OfferCode {
+	
+	code: string;
 	
 	constructor() {
 		
